2419b8f9e1f8610cc4ea223058d088394ad1431e
AlekseyLarkov <djalekseeff@gmail.com>
2023-03-01 16:15:28 +0300

    Решена задача №13

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -4,5 +4,23 @@
 //  78 -> третьей цифры нет
 //  32679 -> 6
 
-
-int 
+int ThirdDigit(int num)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    int thirdnum = num % 10;
+    return thirdnum;
+}
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99)
+{
+    int thirdDigit = ThirdDigit(number);
+    Console.WriteLine($"Третья цифра числа {number} является {thirdDigit}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
