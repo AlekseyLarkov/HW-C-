e337e50414662da95c8d7dfbfdc341d38d946d07
AlekseyLarkov <djalekseeff@gmail.com>
2023-03-01 16:45:29 +0300

    Решена задача №15 без метода

--- Task015/Program.cs
+++ Task015/Program.cs
@@ -3,3 +3,23 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+
+
+Console.WriteLine("Введите целое число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day > 0 && day < 8)
+{
+    if (day == 6 || day == 7)
+    {
+        Console.WriteLine($"Цифра {day} является выходным");
+    }
+    else
+    {
+        Console.WriteLine($"Цифра {day} не является выходным");
+    }
+}
+else 
+{
+    Console.WriteLine($"Цифра {day} не является днем недели");
+}
