e1a49d409e98ef17b164f7d2593d0bd85f8a1a08
AlekseyLarkov <djalekseeff@gmail.com>
2023-03-06 14:58:50 +0300

    Решена задача №19

--- Task019/Program.cs
+++ Task019/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+bool CheckNum(int num1, int num2)
+{
+    return(num1 == num2);
+}
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 9999 && number < 99999)
+{
+    int FirstTwoNum = number / 1000;
+    int LastTwoNum = number % 100;
+    int PreLastNum = LastTwoNum / 10;
+    int LastNum = LastTwoNum % 10;
+    int LastNumCheck = LastNum * 10 + PreLastNum;
+    Console.WriteLine(CheckNum(FirstTwoNum, LastNumCheck) ? "Да" : "Нет");
+}
+else
+{
+    Console.WriteLine("Требуется пятизначное число");
+}
