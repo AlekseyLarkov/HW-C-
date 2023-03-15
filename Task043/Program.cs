b202e125264f6477582f0a20bb7734097682a664
AlekseyLarkov <djalekseeff@gmail.com>
2023-03-15 16:35:56 +0300

    Решена задача №43

--- Task043/Program.cs
+++ Task043/Program.cs
@@ -2,28 +2,28 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-int InputNumber(string text)
+double InputNumber(string text)
 {
     Console.Write($"Введите значение {text}: ");
     string noconvert = Console.ReadLine();
-    int numb = Convert.ToInt32(noconvert);
+    double numb = Convert.ToInt32(noconvert);
     return numb;
 }
-double FindingX(int numK1, int numK2, int numB1, int numB2)
+double FindingX(double numK1, double numK2, double numB1, double numB2)
 {
     double result = (numB2 - numB1) / (numK1 - numK2);
     return result;
 }
-double FindingY(int numK1, int numB1, double numX)
+double FindingY(double numK1, double numB1, double numX)
 {
     double result = (numK1 * numX) + numB1;
     return result;
 }
 
-int b1 = InputNumber("b1");
-int b2 = InputNumber("b2");
-int k1 = InputNumber("k1");
-int k2 = InputNumber("k2");
-double x = FindingX(k1, k2, b1, b2);
-double y = FindingY(k1, b1, x);
+double b1 = InputNumber("b1");
+double b2 = InputNumber("b2");
+double k1 = InputNumber("k1");
+double k2 = InputNumber("k2");
+double x = Math.Round(FindingX(k1, k2, b1, b2), 2);
+double y = Math.Round(FindingY(k1, b1, x), 2);
 Console.WriteLine($"Точка пересечения прямых -> ({x} ; {y})");
