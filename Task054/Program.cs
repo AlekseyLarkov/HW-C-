61b1c766468d59e08b712774be6d9542d26d5d8e
Aleksey Larkov <djalekseeff@gmail.com>
2023-03-24 16:06:45 +0300

    Не работает сортировка в 54 задаче

--- Task054/Program.cs
+++ Task054/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns)
+{
+    int[,] matrx = new int[rows, columns];
+    Random R = new Random();
+    for (int i = 0; i < matrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrx.GetLength(1); j++)
+        {
+            matrx[i, j] = R.Next(0, 10);
+        }
+    }
+    return matrx;
+}
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j], 4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+void SortMatrixRowsFromMaxToMin(int[,] matr)
+{
+    
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int m = 0; m < matr.GetLength(1); m++)
+        {
+            int max = matr[i, m];
+            for (int j = m; j < matr.GetLength(1); j++)
+            {
+                if (matr[i, j] > max)
+                {
+                    max = matr[i, j];
+                }
+                j++;
+            }
+            int buffer = matr[i, m];
+            matr[i, m] = max;
+            max = buffer;
+        }
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = CreateMatrixRndInt(row, column);
+PrintMatrix(matrix);
+Console.WriteLine();
+SortMatrixRowsFromMaxToMin(matrix);
+PrintMatrix(matrix);
